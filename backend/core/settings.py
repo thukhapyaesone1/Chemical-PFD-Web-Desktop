709a5353ec2bb816b742213c1a7550b5b38e29f2
thukha <thukhapyaesone.mmt@gmail.com>
2026-02-11 12:35:53 +0530

    set up for gunicorn and disable debug

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,7 +35,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "chemical-pfd-web-desktop.onrender.com",
